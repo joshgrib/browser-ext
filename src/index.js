43e80e924c9f026aec46328994df4bef684ec246
Josh Gribbon <josh.gribbon@sphereco.com>
2020-02-21 11:18:55 -0500

    Refactor and use the same refresh interval for all plugins

--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ import { version } from '../package.json';
 import messages from './messages.js';
 
 const runPlugins = () => {
-  const url = document.URL;
-  for (let plugin of plugins) {
-    if (plugin.runFor(url)) {
-      console.group(`Josh's extension v${version}`);
-      console.log(`Running '${plugin.name}'...`)
-      plugin.run();
-      console.groupEnd(`Josh's extension v${version}`);
-    }
-  }
+  plugins.filter(p => p.runFor(document.URL)).map(p => {
+    console.group(`Josh's extension v${version}`);
+    console.log(`Running '${p.name}'...`)
+    p.run()
+    setInterval(p.run, 2000);
+    console.groupEnd(`Josh's extension v${version}`);
+  });
 }
 runPlugins();
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    // listen for messages sent from background.js
-    if (request.message === messages.URL_CHANGE) {
-      runPlugins();
-    }
+chrome.runtime.onMessage.addListener(request => {
+  if (request.message === messages.URL_CHANGE) {
+    runPlugins();
+  }
 });
