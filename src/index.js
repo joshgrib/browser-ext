4c6427306471c373f99cde6f63e08cd5ae1f2c96
Josh Gribbon <josh.gribbon@sphereco.com>
2020-02-25 12:31:01 -0500

    Pull name from package.json

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import { name, version } from '../package.json';
 import messages from './messages.js';
 
 const runPlugins = () => {
+  const pluginsToRun = plugins.filter(p => p.runFor(document.URL));
+  if (pluginsToRun.length === 0) return;
   console.group(`${name} v${version}`);
-  plugins.filter(p => p.runFor(document.URL)).map(p => {
+  pluginsToRun.map(p => {
     console.log(`Running '${p.name}'...`)
     p.run()
     setInterval(p.run, 2000);
