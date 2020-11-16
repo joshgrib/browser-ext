e1520466afe4f7d72c906e50c02a30c4da92b4f1
Josh Gribbon <josh.gribbon@sphereco.com>
2020-11-16 11:32:16 -0500

    Refactor, add some comments, fix a bug with the pipeline links

--- dist/popup.js
+++ dist/popup.js
@@ -1,9 +1,23 @@
-const QA_SITE_COUNT = 20;
+/**
+ * This file has the JS code for the extension popup when you click the icon
+ * in the toolbar
+ */
 
+
+/**
+ * Get an array of integers [1, 2, ..., n]
+ * @param {number} n - The amount of elements in the array
+ */
 const range = n => new Array(n).fill().map((x, i) => i+1);
-const QA_SITES = range(QA_SITE_COUNT).map(i => i < 10 ? `QA0${i}` : `QA${i}`);
 
-const makeElem = ({tag, children=[], innerText, click}) => {
+/**
+ * A helper method to create nested DOM elements
+ * @param {string} tag - The tag for the element
+ * @param {array} children - The child elements, also passed into this function
+ * @param {string} innerText - The innerText property for the element
+ * @param {function} click - A `click` event handler
+ */
+const makeElem = ({tag, innerText, click, children=[]}) => {
   const elem = document.createElement(tag);
   if (innerText) {
     elem.innerText = innerText;
@@ -18,10 +32,47 @@ const makeElem = ({tag, children=[], innerText, click}) => {
   return elem;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
+/**
+ * Open the tab(s) for a repo
+ * @param {string} env - The environment to open (QA01-QA20)
+ * @param {string} tab - The tab to open, ('Env' for the deployment, a repo for the pipeline, or 'All')
+ * @param {array} repos - The list of repos that there are pipelines for
+ */
+const openTabs = (env, tab, repos) => {
+  const urls = {
+    deployment: env => `http://sphereboardqa05.spheredemo.corp/${env}`,
+    jenkins: (env, repo) => `http://build-server:8080/blue/organizations/jenkins/mvc%2F${repo}/activity/?branch=${env}`
+  };
+  if (tab === 'Env') {
+    chrome.runtime.sendMessage({
+      urls: [urls.deployment(env)]
+    })
+  } else if (tab === 'All') {
+    alert(JSON.stringify([
+      urls.deployment(env),
+      ...repos.map(r => urls.jenkins(env, r))
+    ]))
+    chrome.runtime.sendMessage({
+      urls: [
+        urls.deployment(env),
+        ...repos.map(r => urls.jenkins(env, r))
+      ]
+    })
+  } else {
+    chrome.runtime.sendMessage({
+      urls: [urls.jenkins(env, tab)]
+    })
+  }
+}
+
+/**
+ * Create the dynamic contents of the popup
+ * @param {number} qaSiteCount - The amount of QA sites to make buttons for
+ */
+const createPopupContent = (qaSiteCount) => {
   const container = document.getElementById('popup-body');
   container.append(makeElem({
-    tag: 'botton',
+    tag: 'button',
     innerText: 'Open extension page to update',
     click: () => {
       chrome.tabs.update({ url: 'chrome://extensions' });
@@ -30,42 +81,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const repos = ['mvc', 'sphereengine', 'backend'];
   const cols = ['Env', ...repos, 'All'];
+  const qaSites = range(qaSiteCount).map(i => i < 10 ? `QA0${i}` : `QA${i}`);
+
   container.append(makeElem({
-    tag: 'table', children: [
+    tag: 'table',
+    children: [
       {
-        tag: 'tr', children: cols.map(innerText => ({ tag: 'th', innerText }))
+        tag: 'tr',
+        children: cols.map(innerText => ({ tag: 'th', innerText }))
       },
-      ...QA_SITES.map(env => ({
-        tag: 'tr', children: cols.map(c => {
-          const innerText = c === 'Env' ? env : c;
+      ...qaSites.map(env => ({
+        tag: 'tr',
+        children: cols.map(col => {
+          const innerText = col === 'Env' ? env : col;
           return {
-            tag: 'td', children: [{
+            tag: 'td',
+            children: [{
               tag: 'button',
               innerText,
-              click: () => {
-                let urls = [];
-                if (c === 'Env') {
-                  chrome.runtime.sendMessage({
-                    urls: [`http://sphereboardqa05.spheredemo.corp/${env}`]
-                  })
-                } else if (c === 'All') {
-                  const urls = [
-                    `http://sphereboardqa05.spheredemo.corp/${env}`,
-                    ...repos.map(r =>
-                      `http://build-server:8080/blue/organizations/jenkins/mvc%252F${r}/activity/?branch=${env}`
-                    )
-                  ]
-                  chrome.runtime.sendMessage({ urls })
-                } else {
-                  chrome.runtime.sendMessage({
-                    urls: [`http://build-server:8080/blue/organizations/jenkins/mvc%252F${c}/activity/?branch=${env}`]
-                  })
-                }
-              }
+              click: () => { openTabs(env, col, repos) }
             }]
           };
         })
       }))
     ]
   }))
+}
+
+const QA_SITE_COUNT = 20;
+
+document.addEventListener('DOMContentLoaded', () => {
+  createPopupContent(QA_SITE_COUNT)
 })
