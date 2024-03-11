9b7f66cf8cb4ac2e0f4c1198c6f3233baae921a9
Josh Gribbon <josh.gribbon@sphereco.com>
2024-03-11 17:40:17 -0400

    Rewrite selectors and add comments to explain

--- src/plugins/gh-clear-action-runs.js
+++ src/plugins/gh-clear-action-runs.js
@@ -6,12 +6,19 @@ export default {
       'Python-workflow.yml',
       'bc-test.yml',
       'Package-File-test.yml',
+      'azbridge-Deploy.yml',
     ];
     return url.includes('github.com') && workflowsToDelete.filter(w => url.includes(w)).length > 0;
   },
   run: function () {
-    document.getElementsByClassName('timeline-comment-action btn-link')[0].click()
-    document.getElementsByClassName('dropdown-item btn-link menu-item-danger btn-link')[0].click()
-    document.getElementsByClassName('Button--danger Button--medium Button Button--fullWidth')[0].click()
+    const selectors = [
+      // open menu for latest action
+      'timeline-comment-action btn-link',
+      // delete workflow run
+      'dropdown-item btn-link menu-item-danger btn-link',
+      // confirm
+      'Button--danger Button--medium Button Button--fullWidth'
+    ];
+    selectors.forEach(s => document.getElementsByClassName(s)[0].click());
   }
 }
